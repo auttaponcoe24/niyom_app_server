c1e87cce41a63959b7f979d66426867c52ccc541
auttapon.c <auttapon.c@siamraj.com>
2024-05-31 17:06:22 +0700

    update init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-require("dotenv");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+import "dotenv";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/", async (req: Request, res: Response) => {
+	const test: string = "Hello world";
+	res.json({ message: test });
+});
+
+// app.use('/auth', )
 
 const PORT = process.env.PORT || "8801";
 app.listen(PORT, () => console.log(`Server is run on PORT: ${PORT}`));
