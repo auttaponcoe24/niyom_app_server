100c834fa8a7e94ff170aeab157e3ebda02c49ed
auttapon.c <auttaponcoe24@gmail.com>
2024-07-02 23:07:32 +0700

    ปรับ package

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,153 +1,114 @@
-import { NextFunction, Request, Response } from "express";
-import prisma from "../models/prisma";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { loginSchema } from "@/validators/auth-validator";
-import createError from "@/utils/create-error";
-import Joi from "joi";
+import { NextFunction, Request, Response } from 'express';
+import prisma from '../models/prisma';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import createError from '@/utils/create-error';
 
-export const register = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	try {
-		const { email, password } = req.body;
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
 
-		// Hash the password
-		const hashPassword = await bcrypt.hash(password, 12);
+    // Hash the password
+    const hashPassword = await bcrypt.hash(password, 12);
 
-		// Create the user in the database
-		const data = await prisma.user.create({
-			data: {
-				email: email,
-				password: hashPassword,
-			},
-		});
+    // Create the user in the database
+    const data = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashPassword,
+      },
+    });
 
-		const payload = {
-			id: data.id,
-			id_passpost: data.id_passpost,
-			firstname: data.firstname,
-			lastname: data.lastname,
-			role: data.role,
-		};
+    const payload = {
+      id: data.id,
+      id_passpost: data.id_passpost,
+      firstname: data.firstname,
+      lastname: data.lastname,
+      role: data.role,
+    };
 
-		const accessToken = jwt.sign(
-			payload,
-			process.env.JWT_SECREY_KEY || "secretKeyRandom",
-			{ expiresIn: process.env.JWT_EXPIRES_IN || "30d" }
-		);
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY || 'secretKeyRandom', { expiresIn: process.env.JWT_EXPIRES_IN || '30d' });
 
-		delete data.password;
-		// Send a response back to the client
-		res.status(201).json({
-			message: "ok",
-			user: data,
-			accessToken: accessToken,
-		});
-	} catch (error) {
-		next(error); // Pass errors to the error handler middleware
-	}
+    delete data.password;
+    // Send a response back to the client
+    res.status(201).json({
+      message: 'ok',
+      user: data,
+      accessToken: accessToken,
+    });
+  } catch (error) {
+    next(error); // Pass errors to the error handler middleware
+  }
 };
 
-export const login = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	try {
-		const {
-			value,
-			error,
-		}: {
-			value: {
-				email: string;
-				password: string;
-			};
-			error: Joi.ValidationError;
-		} = loginSchema.validate(req.body);
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
 
-		if (error) {
-			return next(createError("email incorrect or password incorrect", 400));
-		}
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-		const user = await prisma.user.findUnique({
-			where: {
-				email: value.email,
-			},
-		});
+    if (!user) {
+      return next(createError('mail is not found', 400));
+    }
 
-		if (!user) {
-			return next(createError("mail is not found", 400));
-		}
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(createError('password incorrect', 400));
+    }
 
-		const isMatch = await bcrypt.compare(value.password, user.password);
-		if (!isMatch) {
-			return next(createError("password incorrect", 400));
-		}
+    const payload = {
+      id: user.id,
+      id_passpost: user.id_passpost,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      role: user.role,
+    };
 
-		const payload = {
-			id: user.id,
-			id_passpost: user.id_passpost,
-			firstname: user.firstname,
-			lastname: user.lastname,
-			role: user.role,
-		};
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY || 'secretKeyRandom', { expiresIn: process.env.JWT_EXPIRES_IN || '30d' });
 
-		const accessToken = jwt.sign(
-			payload,
-			process.env.JWT_SECRET_KEY || "secretKeyRandom",
-			{ expiresIn: process.env.JWT_EXPIRES_IN || "30d" }
-		);
+    delete user.password;
 
-		delete user.password;
-
-		res.status(200).json({
-			message: "ok",
-			user: user,
-			accessToken: accessToken,
-		});
-	} catch (error) {
-		next(error);
-	}
+    res.status(200).json({
+      message: 'ok',
+      user: user,
+      accessToken: accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getProfile = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	try {
-		res.status(200).json({ message: "ok", user: req.user });
-	} catch (error) {
-		next(error);
-	}
+export const getProfile = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.status(200).json({ message: 'ok', user: req.user });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const editProfile = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	try {
-		const { id } = req.user;
-		const { firstname, lastname, id_passpost, address } = req.body;
+export const editProfile = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.user;
+    const { firstname, lastname, id_passpost, address } = req.body;
 
-		const findUser = await prisma.user.update({
-			where: {
-				id: id,
-			},
-			data: {
-				firstname,
-				lastname,
-				id_passpost,
-				address,
-			},
-		});
+    const findUser = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        firstname,
+        lastname,
+        id_passpost,
+        address,
+      },
+    });
 
-		res.status(201).json({ message: "ok", result: findUser });
-	} catch (error) {
-		next(error);
-	}
+    res.status(201).json({ message: 'ok', result: findUser });
+  } catch (error) {
+    next(error);
+  }
 };
--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,56 +1,49 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
-import prisma from "@/models/prisma";
-import { NextFunction, Response, Request } from "express";
-import createError from "@/utils/create-error";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import prisma from '@/models/prisma';
+import { NextFunction, Response, Request } from 'express';
+import createError from '@/utils/create-error';
 
 interface IPayload extends JwtPayload {
-	id: string;
-	id_passpost: string;
-	firstname: string;
-	lastname: string;
-	role: "OWNER" | "CUSTOMER";
+  id: string;
+  id_passpost: string;
+  firstname: string;
+  lastname: string;
+  role: 'OWNER' | 'CUSTOMER';
 }
 
-const authMiddleware = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	try {
-		const authorization = req.headers.authorization;
-		if (!authorization) {
-			return next(createError("unauthenticated", 401));
-		}
+const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return next(createError('unauthenticated', 401));
+    }
 
-		const token = authorization.split(" ")[1];
+    const token = authorization.split(' ')[1];
 
-		const payload = jwt.verify(
-			token,
-			process.env.JWT_SECRET_KEY || "secretKeyRandom"
-		) as IPayload;
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY || 'secretKeyRandom') as IPayload;
 
-		// const { id_passpost } = payload;
+    // const { id_passpost } = payload;
 
-		const user = await prisma.user.findUnique({
-			where: {
-				id: payload.id,
-			},
-		});
+    const user = await prisma.user.findUnique({
+      where: {
+        id: payload.id,
+      },
+    });
 
-		if (!user) {
-			return next(createError("unauthenticated", 401));
-		}
+    if (!user) {
+      return next(createError('unauthenticated', 401));
+    }
 
-		delete user.password;
+    delete user.password;
 
-		req.user = user;
-		next();
-	} catch (error) {
-		if (error.name === "TokenExpiredError" || "JsonWebTokenError") {
-			error.statusCode = 401;
-		}
-		next(error);
-	}
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError' || 'JsonWebTokenError') {
+      error.statusCode = 401;
+    }
+    next(error);
+  }
 };
 
 export default authMiddleware;
